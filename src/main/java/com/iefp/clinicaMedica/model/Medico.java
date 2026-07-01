c36d4da426d2f9cd8d6d62915e1ba2d53ad7fab2
Daniela Torres Almeida <danielarosadolealtorresalmeida@gmail.com>
2026-07-01 14:41:03 +0100

    Adicionei comentários de documentação ao modelo Medico
    Adicionei comentários exaustivos que documentem todos os campos, construtores e métodos da classe Medico, de forma a melhorar a legibilidade e a facilidade de manutenção do código. Reorganizar também os métodos getter/setter para um melhor agrupamento lógico.

--- src/main/java/com/iefp/clinicaMedica/model/Medico.java
+++ src/main/java/com/iefp/clinicaMedica/model/Medico.java
@@ -11,46 +11,57 @@ import jakarta.persistence.OneToOne;
 @Entity
 public class Medico {
 
+    // Identificador único do médico
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Especialidade do médico, por exemplo: Cardiologia, Pediatria, etc.
     private String especialidade;
 
-    // Composição com Utilizador
+    // Relação entre Médico e Utilizador
+    // Cada médico tem um utilizador associado
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "utilizador_id", referencedColumnName = "id")
     private Utilizador utilizador;
 
+    // Construtor vazio obrigatório para o JPA
     public Medico() {
     }
 
+    // Construtor com todos os campos
     public Medico(Long id, String especialidade, Utilizador utilizador) {
         this.id = id;
         this.especialidade = especialidade;
         this.utilizador = utilizador;
     }
 
+    // Devolve o ID do médico
     public Long getId() {
         return id;
     }
 
-    public String getEspecialidade() {
-        return especialidade;
-    }
-
-    public Utilizador getUtilizador() {
-        return utilizador;
-    }
-
+    // Altera o ID do médico
     public void setId(Long id) {
         this.id = id;
     }
 
+    // Devolve a especialidade do médico
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    // Altera a especialidade do médico
     public void setEspecialidade(String especialidade) {
         this.especialidade = especialidade;
     }
 
+    // Devolve o utilizador associado ao médico
+    public Utilizador getUtilizador() {
+        return utilizador;
+    }
+
+    // Altera o utilizador associado ao médico
     public void setUtilizador(Utilizador utilizador) {
         this.utilizador = utilizador;
     }
